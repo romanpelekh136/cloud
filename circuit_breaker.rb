e846e06b1671e94b947ecb674ef71a575eb7d1a4
romanpelekh136 <rpeleh19@gmail.com>
2025-10-24 01:10:39 +0000

    Add Circuit Breaker implementation and RSpec setup

---
+++ circuit_breaker.rb
@@ -0,0 +1,86 @@
+class CircuitBreakerError < StandardError; end
+
+class CircuitBreaker
+  attr_reader :state
+
+  def initialize(failure_threshold:, recovery_timeout:)
+    @failure_threshold = failure_threshold
+    @recovery_timeout = recovery_timeout
+
+    @state = :closed
+    @failure_count = 0
+    @last_failure_time = nil
+  end
+
+  def execute
+    case @state
+    when :closed
+      handle_closed_state { yield }
+    when :open
+      handle_open_state { yield }
+    when :half_open
+      handle_half_open_state { yield }
+    end
+  end
+
+  private
+
+  def reset
+    @failure_count = 0
+    @failure_count = 0
+    @last_failure_time = nil
+  end
+
+  def record_failure
+    @failure_count += 1
+    if @failure_count >= @failure_threshold
+      trip
+    end
+  end
+
+  def trip
+    change_state(:open)
+
+    @last_failure_time = Time.now
+  end
+
+  def change_state(new_state)
+    unless @state == new_state
+      puts "[CircuitBreaker] Стан: #{@state} -> #{new_state}"
+      @state = new_state
+    end
+  end
+
+
+
+  def handle_closed_state
+    begin
+      result = yield
+      reset; result
+    rescue StandardError => e
+      record_failure; raise e
+    end
+  end
+
+  def handle_open_state
+    if Time.now - @last_failure_time >= @recovery_timeout
+      change_state(:half_open)
+      # Якщо так, ОДРАЗУ пробуємо
+      handle_half_open_state { yield }
+    else
+      # Якщо ні, "падаємо"
+      raise CircuitBreakerError, "Circuit is OPEN. Fast-failing."
+    end
+  end
+
+  def handle_half_open_state
+    begin
+      result = yield
+      # Успіх? Чудово, закриваємо
+      reset; result
+    rescue StandardError => e
+      # Помилка? Погано, знову відкриваємо
+      trip; raise e
+    end
+  end
+end
