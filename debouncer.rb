69de3e3af4a8b93625e86d9bb5562be82d44efd9
romanpelekh136 <rpeleh19@gmail.com>
2025-10-24 15:38:08 +0000

    Add Debouncer class implementation and RSpec tests for execution behavior

---
+++ debouncer.rb
@@ -0,0 +1,22 @@
+require 'thread' 
+
+class Debouncer
+  def initialize(wait_time)
+    @wait_time = wait_time
+    @timer_thread = nil    
+    @mutex = Mutex.new
+  end
+
+  def execute
+    @mutex.synchronize do
+      
+      @timer_thread.kill if @timer_thread&.alive?
+
+      @timer_thread = Thread.new do
+        sleep @wait_time
+        
+        yield
+      end
+    end
+  end
+end
---
+++ spec/debounce_spec.rb
@@ -0,0 +1,34 @@
+require 'rspec'
+require_relative '../debouncer'
+
+RSpec.describe Debouncer do
+
+  let(:mock_service) { double("MockService") }
+
+  let(:wait_time) {0.1}
+
+  let(:debouncer) {described_class.new(wait_time)}
+
+  it 'execute one time after wait time' do
+    expect(mock_service).to receive(:call).once
+
+    debouncer.execute { mock_service.call }
+
+    sleep wait_time + 0.05
+  end
+
+  it 'execute only last req if requests are too frequent' do 
+    expect(mock_service).to receive(:action1).never
+    expect(mock_service).to receive(:action2).once
+
+    debouncer.execute { mock_service.action1 }
+
+    sleep wait_time / 2
+
+    debouncer.execute { mock_service.action2 }
+
+    sleep wait_time + 0.05
+
+  end
+
+end
