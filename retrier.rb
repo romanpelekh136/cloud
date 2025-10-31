518dcd5ed742e72d451ecb9078f9f9340b06a58f
romanpelekh136 <rpeleh19@gmail.com>
2025-10-31 15:54:23 +0000

    Fix retry logic in Retrier class to handle exceptions correctly and improve readability

--- retrier.rb
+++ retrier.rb
@@ -7,10 +7,22 @@ class Retrier
 
   def execute
     @max_attempts.times do |attempt|
+
+      current_attempt = attempt + 1
+
       begin
         return yield
-      rescue 
-        
+      rescue StandardError => e 
+        if current_attempt => max_attempts
+
+          puts "[Retry] All #{@max_attempts} tries failed."
+          raise e
+
+        else
+          
+          sleep @delay
+          
+        end
       end
       
     end
