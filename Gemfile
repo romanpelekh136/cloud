3f94d056adf5d207de5cdc97bc8811cf917f764c
romanpelekh136 <rpeleh19@gmail.com>
2025-10-31 16:20:45 +0000

    Update Gemfile and Gemfile.lock to include RSpec components; adjust delay in Retrier spec for improved test accuracy

--- Gemfile
+++ Gemfile
@@ -4,4 +4,8 @@ source 'https://rubygems.org'
 
 gem 'rspec'
 
+gem 'rspec-core'
+gem 'rspec-expectations'
+gem 'rspec-mocks'
+
 gem 'rubocop'
--- spec/retrier_spec.rb
+++ spec/retrier_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../retrier'
 
 RSpec.describe Retrier do
-  let(:delay) { 0.5 }
+  let(:delay) { 0.1 }
 
   let(:max_attempts) { 3 }
 
@@ -10,7 +10,13 @@ RSpec.describe Retrier do
 
   let(:retrier) { described_class.new(max_attempts: max_attempts ,delay: delay) }
 
-  
+  it 'success fro, first try' do
+    allow(mock_service).to receive(:call).and_return("Success")
+
+    result = retrier.execute { mock_service.call }
+    expect(result).to eq("Success")
+    expect(mock_service).to have_received(:call).once
+  end
 
 
 
